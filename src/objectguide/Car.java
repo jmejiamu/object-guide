787c1de01b7f80c4571d56c49ae160fb46a95ae6
Jose <jmejiamu@gmail.com>
2021-09-30 17:10:16 -0700

    classses

---
+++ src/objectguide/Car.java
@@ -0,0 +1,19 @@
+package objectguide;
+
+/**
+ *
+ * @author JoseMejia62
+ */
+public class Car {
+    private String model;
+    private int year;
+    private String color;
+    
+    public void carBreak(){
+        System.out.println("Break");
+    }
+    
+    public void carDrive(){
+        System.out.println("Drive");
+    }
+}
--- src/objectguide/Main.java
+++ src/objectguide/Main.java
@@ -1,21 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package objectguide;
 
 /**
  *
- * @author joch_
+ * @author JoseMejia62
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Car carOne = new Car();
+        
+        carOne.carBreak();
+        carOne.carDrive();
     }
     
 }
