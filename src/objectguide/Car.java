7713fa3fee57497ccdeb1be7380c7726dbaba548
Jose <jmejiamu@gmail.com>
2021-09-30 19:34:18 -0700

    constructor

--- src/objectguide/Car.java
+++ src/objectguide/Car.java
@@ -5,9 +5,23 @@ package objectguide;
  * @author JoseMejia62
  */
 public class Car {
+    
+    // - Instance Variables
     private String model;
     private int year;
     private String color;
+
+    // - Default Constructor
+    public Car() {    }
+
+    // Constructor with parameters
+    public Car(String model, int year, String color) {
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+    
+    
     
     public void carBreak(){
         System.out.println("Break");
