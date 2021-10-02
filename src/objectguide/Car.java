e2fe5ccf0c97e4156129640fc5727db759ad9edc
Jose <jmejiamu@gmail.com>
2021-10-01 21:10:59 -0700

    getter and setter

--- src/objectguide/Car.java
+++ src/objectguide/Car.java
@@ -20,6 +20,30 @@ public class Car {
         this.year = year;
         this.color = color;
     }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
     
     
     
